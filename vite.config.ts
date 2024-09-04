bf2bef055359219f5e9b2003ab1d0bb3b218b3d4
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-09-04 20:36:12 +0600

    345

--- vite.config.ts
+++ vite.config.ts
@@ -4,45 +4,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "@svgr/rollup";
 import dotenv from "dotenv";
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-dotenv.config();
 
-const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "autoUpdate",
-  manifest: {
-    name: "PetSeeker",
-    short_name: "PetSeeker",
-    description:
-      "PetSeeker is an application that helps you find homes for animals",
-    theme_color: "#ffffff",
-    background_color: "#ffffff",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    icons: [
-      {
-        src: "./src/shared/assets/pwa87x87.png",
-        sizes: "87x87",
-        type: "image/png",
-      },
-      {
-        src: "./src/shared/assets/pwa120x120.png",
-        sizes: "120x120",
-        type: "image/png",
-      },
-      {
-        src: "./src/shared/assets/pwa180x180.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-      {
-        src: "./src/shared/assets/pwa1024x1024.png",
-        sizes: "1024x1024",
-        type: "image/png",
-      },
-    ],
-  },
-};
+dotenv.config();
 
 export default defineConfig({
   resolve: {
@@ -56,7 +19,7 @@ export default defineConfig({
       "@app": path.resolve(__dirname, "./src/app"),
     },
   },
-  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
+  plugins: [react(), svgr()],
 
   server: {
     proxy: {
